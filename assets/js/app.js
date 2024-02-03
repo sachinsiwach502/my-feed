d80a0571c1f445890957cac2edcd55bc8a5f116d
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-02-03 09:49:18 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+// let circle = document.querySelector(".inpu-box");
+// let radio = document.querySelector(".input-box");
+// circle.addEventListener("click", function () {
+//     if (radio === checked) {
+//         circle.style.backgroundColor = "black";
+//     } else {
+//         circle.style.backgroundColor = "white";
+//     }
+// });
+// if (radio === checked) {
+//     onclick.circle.backgroundColor = "black"
+// } else {
+//     onclick.circle.backgroundColor = "white"
+// }
+
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});
+let sp = 1;
+let id = document.getElementsByClassName(".following");
+id.addEventListener("click", function () {
+    if (sp == 1) {
+        id.forEach(e => e.innerHTML = "following");
+        sp = 2;
+    } else {
+        id.forEach(e => e.innerHTML = "follow");
+        sp = 1;
+    }
+});
